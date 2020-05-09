f64158474a2a86b6fe772dbfc62149701ffd17e2
Richie <richiejcasey@gmail.com>
2020-05-09 11:20:15 +0000

    Add pagination to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_200514) do
+ActiveRecord::Schema.define(version: 2020_05_08_221817) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_05_08_200514) do
     t.string "email"
     t.string "timestamps"
     t.string "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
