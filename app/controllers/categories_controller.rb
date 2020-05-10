bb690cee0f1bc0e1ff6c6ce011f825afb980ef75
Richie <richiejcasey@gmail.com>
2020-05-10 13:36:48 +0000

    add categories model and tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,30 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+  @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  private # Everything below this will be private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
